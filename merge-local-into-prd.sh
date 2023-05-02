6cf6d9078e1ca88d7a8de4150cb31132135628a4
hiroya-akeoka <mowoarm232@gmail.com>
2023-05-03 07:56:21 +0900

    :recycle:定数を環境変数として.envに切り出し

--- merge-local-into-prd.sh
+++ merge-local-into-prd.sh
@@ -1,40 +1,14 @@
 #!/bin/sh
-NOW=`date +"%Y%m%d_%H%M"`
-SEARCH="http://localhost:ポート番号"
-REPLACE="https://ドメイン名"
-
-#dumpファイルの保存先
-BAK_REMOTE="/tmp/prd.bk.$NOW.sql"
-BAK_LOCAL="/tmp/local.bk.$NOW.sql"
-
-PRD_SERVER_HOST="リモートサーバーホスト"
-PRD_SERVER_PORT="リモートサーバーポート"
-
-SSH_KEY_FILE="sshキーファイルのパス"
-
-LOCAL_PUBLIC_DIR="public_html"
-PRD_PUBLIC_DIR="${PRD_SERVER_HOST}:public_htmlの絶対パス"
-
-LOCAL_DB_HOST="127.0.0.1"
-LOCAL_DB_PORT="DBのホスト側ポート"
-LOCAL_DB_NAME="ローカルのDB名"
-LOCAL_DB_USER="ローカルのユーザ名"
-LOCAL_DB_PASSWORD="ローカルのパスワード"
-
-PRD_DB_HOST="リモートのDBホスト名"
-PRD_DB_NAME="リモートのDB名"
-PRD_DB_USER="リモートのユーザ名"
-PRD_DB_PASSWORD="リモートのパスワード"
-
+source .env
 
 echo "【本番のDBをバックアップ】"
-ssh $PRD_SERVER_HOST -p $PRD_SERVER_PORT -i $SSH_KEY_FILE mysqldump -u$PRD_DB_USER -p$PRD_DB_PASSWORD -h$PRD_DB_HOST $PRD_DB_NAME --no-tablespaces > $BAK_REMOTE
+ssh $PRD_SERVER_HOST -p $PRD_SERVER_PORT mysqldump -u$PRD_DB_USER -p$PRD_DB_PASSWORD -h$PRD_DB_HOST $PRD_DB_NAME --no-tablespaces > $BAK_PRD
 #ファイルがない場合は終了
-if [ ! -s $BAK_REMOTE ]; then
-  echo "dump failed: check $BAK_REMOTE"
+if [ ! -s $BAK_PRD ]; then
+  echo "dump failed: check $BAK_PRD"
   exit
 fi
-echo "【完了】"
+echo "【完了】\n"
 
 echo "【ローカルのDBをダンプ】"
 mysqldump -u$LOCAL_DB_USER -p$LOCAL_DB_PASSWORD -h$LOCAL_DB_HOST -P$LOCAL_DB_PORT $LOCAL_DB_NAME --column-statistics=0 --no-tablespaces > $BAK_LOCAL
@@ -43,7 +17,7 @@ if [ ! -s $BAK_LOCAL ]; then
   echo "dump failed: check $BAK_LOCAL"
   exit
 fi
-echo "【完了】"
+echo "【完了】\n"
 
 echo "【ローカルのpublic_htmlを本番にコピー】"
 rsync --checksum -rv -e "ssh -p ${PRD_SERVER_PORT}" $LOCAL_PUBLIC_DIR/* $PRD_PUBLIC_DIR/
@@ -52,13 +26,16 @@ scp -P $PRD_SERVER_PORT -r $LOCAL_PUBLIC_DIR/.user.ini $PRD_PUBLIC_DIR
 echo "【完了】"
 
 echo "【wp-config.phpの内容を本番環境のものに書き換え】"
-scp -P $PRD_SERVER_PORT -r wp-config-prd.php $PRD_PUBLIC_DIR/wp-config.php
-echo "【完了】"
+scp -P $PRD_SERVER_PORT -r \
+  wp-config-prd.php $PRD_PUBLIC_DIR/wp-config.php
+echo "【完了】\n"
 
 echo "【本番のDBをローカルのDBで上書き】"
-ssh $PRD_SERVER_HOST -p $PRD_SERVER_PORT mysql -u$PRD_DB_USER -p$PRD_DB_PASSWORD -h$PRD_DB_HOST $PRD_DB_NAME < $BAK_LOCAL
-echo "【完了】"
+ssh $PRD_SERVER_HOST -p $PRD_SERVER_PORT \
+  mysql -u$PRD_DB_USER -p$PRD_DB_PASSWORD -h$PRD_DB_HOST $PRD_DB_NAME < $BAK_LOCAL
+echo "【完了】\n"
 
 echo "【本番のDB内のドメイン部分を書き換え】"
-ssh $PRD_SERVER_HOST -p $PRD_SERVER_PORT /usr/bin/php7.3 srdb.cli.php -h $PRD_DB_HOST -u $PRD_DB_USER -p $PRD_DB_PASSWORD -n $PRD_DB_NAME -s $SEARCH -r $REPLACE
-echo "【完了】"
+ssh $PRD_SERVER_HOST -p $PRD_SERVER_PORT \
+  /usr/bin/php7.3 srdb.cli.php -h $PRD_DB_HOST -u $PRD_DB_USER -p $PRD_DB_PASSWORD -n $PRD_DB_NAME -s $LOCAL_DOMAIN -r $PRD_DOMAIN
+echo "【完了】\n"
--- merge-prd-into-local.sh
+++ merge-prd-into-local.sh
@@ -1,31 +1,5 @@
 #!/bin/sh
-NOW=`date +"%Y%m%d_%H%M"`
-SEARCH="https://ドメイン名"
-REPLACE="http://localhost:ポート番号"
-
-#dumpファイルの保存先
-BAK_REMOTE="/tmp/prd.bk.$NOW.sql"
-BAK_LOCAL="/tmp/local.bk.$NOW.sql"
-
-PRD_SERVER_HOST="リモートサーバーホスト"
-PRD_SERVER_PORT="リモートサーバーポート"
-
-SSH_KEY_FILE="sshキーファイルのパス"
-
-LOCAL_PUBLIC_DIR="public_html"
-PRD_PUBLIC_DIR="${PRD_SERVER_HOST}:public_htmlの絶対パス"
-
-LOCAL_DB_HOST="127.0.0.1"
-LOCAL_DB_PORT="DBのホスト側ポート"
-LOCAL_DB_NAME="ローカルのDB名"
-LOCAL_DB_USER="ローカルのユーザ名"
-LOCAL_DB_PASSWORD="ローカルのパスワード"
-
-PRD_DB_HOST="リモートのDBホスト名"
-PRD_DB_NAME="リモートのDB名"
-PRD_DB_USER="リモートのユーザ名"
-PRD_DB_PASSWORD="リモートのパスワード"
-
+source .env
 
 echo "【ローカルのDBをバックアップ】"
 mysqldump -u$LOCAL_DB_USER -p$LOCAL_DB_PASSWORD -h$LOCAL_DB_HOST -P$LOCAL_DB_PORT $LOCAL_DB_NAME --column-statistics=0 --no-tablespaces > $BAK_LOCAL
@@ -34,16 +8,17 @@ if [ ! -s $BAK_LOCAL ]; then
   echo "dump failed: check $BAK_LOCAL"
   exit
 fi
-echo "【完了】"
+echo "【完了】\n"
 
 echo "【本番のDBをダンプ】"
-ssh $PRD_SERVER_HOST -p $PRD_SERVER_PORT -i $SSH_KEY_FILE mysqldump -u$PRD_DB_USER -p$PRD_DB_PASSWORD -h$PRD_DB_HOST $PRD_DB_NAME --no-tablespaces > $BAK_REMOTE
+ssh $PRD_SERVER_HOST -p $PRD_SERVER_PORT \
+  mysqldump -u$PRD_DB_USER -p$PRD_DB_PASSWORD -h$PRD_DB_HOST $PRD_DB_NAME --no-tablespaces > $BAK_PRD
 #ファイルがない場合は終了
-if [ ! -s $BAK_REMOTE ]; then
-  echo "dump failed: check $BAK_REMOTE"
+if [ ! -s $BAK_PRD ]; then
+  echo "dump failed: check $BAK_PRD"
   exit
 fi
-echo "【完了】"
+echo "【完了】\n"
 
 echo "【本番のpublic_htmlをローカルにコピー】"
 rsync --checksum -rv -e "ssh -p ${PRD_SERVER_PORT}" --exclude 'wp-content/uploads/file-backup' --exclude 'wp-content/uploads/database-backup' $PRD_PUBLIC_DIR/\* $LOCAL_PUBLIC_DIR/
@@ -52,20 +27,13 @@ scp -P $PRD_SERVER_PORT -r $PRD_PUBLIC_DIR/.user.ini $LOCAL_PUBLIC_DIR
 echo "【完了】"
 
 echo "【wp-config.phpの内容をローカル環境のものに書き換え】"
-\cp -f  wp-config-local.php $LOCAL_PUBLIC_DIR/wp-config.php
-echo "【完了】"
+cp -f  wp-config-local.php $LOCAL_PUBLIC_DIR/wp-config.php
+echo "【完了】\n"
 
 echo "【ローカルのDBを本番のDBで上書き】"
-mysql -u$LOCAL_DB_USER -p$LOCAL_DB_PASSWORD -h$LOCAL_DB_HOST -P$LOCAL_DB_PORT $LOCAL_DB_NAME < $BAK_REMOTE
-echo "【完了】"
+mysql -u$LOCAL_DB_USER -p$LOCAL_DB_PASSWORD -h$LOCAL_DB_HOST -P$LOCAL_DB_PORT $LOCAL_DB_NAME < $BAK_PRD
+echo "【完了】\n"
 
 echo "【ローカルのDB内のドメイン部分を書き換え】"
-php srdb.cli.php -h $LOCAL_DB_HOST -P $LOCAL_DB_PORT -u $LOCAL_DB_USER -p $LOCAL_DB_PASSWORD -n $LOCAL_DB_NAME -s $SEARCH -r $REPLACE
-echo "【完了】"
-
-
-
-
-
-
-
+php srdb.cli.php -h $LOCAL_DB_HOST -P $LOCAL_DB_PORT -u $LOCAL_DB_USER -p $LOCAL_DB_PASSWORD -n $LOCAL_DB_NAME -s $PRD_DOMAIN -r $LOCAL_DOMAIN
+echo "【完了】\n"
