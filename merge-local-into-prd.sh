bc27a33e31e254cbc2d276ff323c70dd4e157b46
hiroya-akeoka <mowoarm232@gmail.com>
2024-04-03 15:34:05 +0900

    ♻️ 同期処理のリファクタリング
    - 処理状況に関する標準出力の追加・文言修正
    - 横長のコマンドを適宜改行
    - 処理のブロックを並べ替え、バックアップ処理・同期処理・後処理の3つに分割
    - 本番のDBダンプとローカルDBへのコピー処理をまとめた
    - 機能していない例外処理を一旦削除

--- merge-local-into-prd.sh
+++ merge-local-into-prd.sh
@@ -3,57 +3,78 @@
 
 . ./.env
 
-echo "【本番のみで必要なプラグインの有効化】"
+echo "------------------------------前処理　開始------------------------------"
+echo "【本番環境で必要なプラグインの有効化】"
 sh activate-plugin-local.sh
 printf "【完了】\n\n"
+echo "------------------------------前処理　完了------------------------------"
 
-echo "【本番のDBをバックアップ】"
-ssh "$PRD_SSH_DESTINATION" -p "$PRD_SSH_PORT" mysqldump -u"$PRD_DB_USER" -p"$PRD_DB_PASSWORD" -h"$PRD_DB_HOST" "$PRD_DB_NAME" --no-tablespaces >"$PRD_DB_DUMP_FILE_PATH"
-#ファイルがない場合は終了
-if [ ! -s "$PRD_DB_DUMP_FILE_PATH" ]; then
-  echo "dump failed!"
-  exit
-fi
+echo "------------------------------本番環境バックアップ　開始------------------------------"
+echo "【DBをバックアップ】"
+ssh "$PRD_SSH_DESTINATION" -p "$PRD_SSH_PORT" \
+  mysqldump \
+  -u"$PRD_DB_USER" \
+  -p"$PRD_DB_PASSWORD" \
+  -h"$PRD_DB_HOST" \
+  "$PRD_DB_NAME" --no-tablespaces >"$PRD_DB_DUMP_FILE_PATH"
 printf "【完了】\n\n"
 
-echo "【ローカルのDBをダンプ】"
-mysqldump -u"$LOCAL_DB_USER" -p"$LOCAL_DB_PASSWORD" -h"$LOCAL_DB_HOST" -P"$LOCAL_DB_PORT" "$LOCAL_DB_NAME" --column-statistics=0 --no-tablespaces >"$LOCAL_DB_DUMP_FILE_PATH"
-#ファイルがない場合は終了
-if [ ! -s "$LOCAL_DB_DUMP_FILE_PATH" ]; then
-  echo "dump failed!"
-  exit
-fi
-printf "【完了】\n\n"
-
-echo "【本番のpublic_htmlをバックアップ】"
+echo "【public_htmlをバックアップ】"
 rsync --checksum -arv --delete \
   -e "ssh -p \"$PRD_SSH_PORT\"" \
-  --exclude "$STG_DOMAIN" --exclude "$WORDPRESS_CACHE_DIR_PATH" --exclude "$BACKWPUP_LOG_DIR_PATH" --exclude "$BACKWPUP_TEMP_DIR_PATH" \
+  --exclude "$STG_DOMAIN" \
+  --exclude "$WORDPRESS_CACHE_DIR_PATH" \
+  --exclude "$BACKWPUP_LOG_DIR_PATH" \
+  --exclude "$BACKWPUP_TEMP_DIR_PATH" \
   "$PRD_PUBLIC_DIR_PATH_WITH_DESTINATION"/ "$PRD_FILE_BACKUP_DIR_PATH"/
 printf "【完了】\n\n"
+echo "------------------------------本番環境バックアップ　完了------------------------------"
 
-echo "【ローカルのpublic_htmlを本番にコピー】"
+echo "------------------------------ローカル→本番環境同期　開始------------------------------"
+echo "【DBを同期】"
+mysqldump \
+  -u"$LOCAL_DB_USER" \
+  -p"$LOCAL_DB_PASSWORD" \
+  -h"$LOCAL_DB_HOST" \
+  -P"$LOCAL_DB_PORT" \
+  "$LOCAL_DB_NAME" \
+  --column-statistics=0 --no-tablespaces |
+  ssh "$PRD_SSH_DESTINATION" -p "$PRD_SSH_PORT" \
+    mysql \
+    -u"$PRD_DB_USER" \
+    -p"$PRD_DB_PASSWORD" \
+    -h"$PRD_DB_HOST" \
+    "$PRD_DB_NAME"
+printf "【完了】\n\n"
+
+echo "【ドメインを置換】"
+ssh "$PRD_SSH_DESTINATION" -p "$PRD_SSH_PORT" \
+  /usr/bin/php7.3 srdb.cli.php \
+  -h "$PRD_DB_HOST" \
+  -u "$PRD_DB_USER" \
+  -p "$PRD_DB_PASSWORD" \
+  -n "$PRD_DB_NAME" \
+  -s "http://${LOCAL_DOMAIN}" -r "https://${PRD_DOMAIN}"
+printf "【完了】\n\n"
+
+echo "【public_htmlを同期】"
 rsync --checksum -arv --delete \
   -e "ssh -p ${PRD_SSH_PORT}" \
-  --exclude "$STG_DOMAIN" --exclude "$WORDPRESS_CACHE_DIR_PATH" --exclude "$BACKWPUP_LOG_DIR_PATH" --exclude "$BACKWPUP_TEMP_DIR_PATH" \
+  --exclude "$STG_DOMAIN" \
+  --exclude "$WORDPRESS_CACHE_DIR_PATH" \
+  --exclude "$BACKWPUP_LOG_DIR_PATH" \
+  --exclude "$BACKWPUP_TEMP_DIR_PATH" \
   "$LOCAL_PUBLIC_DIR_PATH"/ "$PRD_PUBLIC_DIR_PATH_WITH_DESTINATION"/
 printf "【完了】\n\n"
 
-echo "【wp-config.phpの内容を本番環境のものに書き換え】"
+echo "【wp-config.phpを置換】"
 scp -P "$PRD_SSH_PORT" \
   ./wp-config-prd.php "$PRD_PUBLIC_DIR_PATH_WITH_DESTINATION"/wp-config.php
 printf "【完了】\n\n"
+echo "------------------------------ローカル→本番環境同期　開始------------------------------"
 
-echo "【本番のDBをローカルのDBで上書き】"
-ssh "$PRD_SSH_DESTINATION" -p "$PRD_SSH_PORT" \
-  mysql -u"$PRD_DB_USER" -p"$PRD_DB_PASSWORD" -h"$PRD_DB_HOST" "$PRD_DB_NAME" <"$LOCAL_DB_DUMP_FILE_PATH"
-printf "【完了】\n\n"
-
-echo "【本番のDB内のドメイン部分を書き換え】"
-ssh "$PRD_SSH_DESTINATION" -p "$PRD_SSH_PORT" \
-  /usr/bin/php7.3 srdb.cli.php -h "$PRD_DB_HOST" -u "$PRD_DB_USER" -p "$PRD_DB_PASSWORD" -n "$PRD_DB_NAME" -s "http://${LOCAL_DOMAIN}" -r "https://${PRD_DOMAIN}"
-printf "【完了】\n\n"
-
-echo "【本番以外では不要なプラグインの無効化】"
+echo "------------------------------後処理　開始------------------------------"
+echo "【ローカル環境では不要なプラグインの無効化】"
 sh deactivate-plugin-local.sh
 printf "【完了】\n\n"
+echo "------------------------------後処理　完了------------------------------"
--- merge-prd-into-stg.sh
+++ merge-prd-into-stg.sh
@@ -4,46 +4,72 @@
 
 . ./.env
 
-echo "【ステージングのDBをバックアップ】"
+echo "------------------------------ステージング環境バックアップ　開始------------------------------"
+echo "【DBをバックアップ】"
 ssh "$STG_SSH_DESTINATION" -p "$STG_SSH_PORT" \
-  mysqldump -u"$STG_DB_USER" -p"$STG_DB_PASSWORD" -h"$STG_DB_HOST" "$STG_DB_NAME" --no-tablespaces >"$STG_DB_DUMP_FILE_PATH"
-#ファイルがない場合は終了
-if [ ! -s "$STG_DB_DUMP_FILE_PATH" ]; then
-  echo "dump failed!"
-  exit
-fi
+  mysqldump \
+  -u"$STG_DB_USER" \
+  -p"$STG_DB_PASSWORD" \
+  -h"$STG_DB_HOST" \
+  "$STG_DB_NAME" \
+  --no-tablespaces >"$STG_DB_DUMP_FILE_PATH"
 printf "【完了】\n\n"
 
-echo "【本番のDBをダンプ】"
-ssh "$PRD_SSH_DESTINATION" -p "$PRD_SSH_PORT" \
-  mysqldump -u"$PRD_DB_USER" -p"$PRD_DB_PASSWORD" -h"$PRD_DB_HOST" "$PRD_DB_NAME" --no-tablespaces >"$PRD_DB_DUMP_FILE_PATH"
-#ファイルがない場合は終了
-if [ ! -s "$PRD_DB_DUMP_FILE_PATH" ]; then
-  echo "dump failed!"
-  exit
-fi
-printf "【完了】\n\n"
-
-echo "【ステージングのpublic_htmlをバックアップ】"
+echo "【public_htmlをバックアップ】"
 rsync --checksum -arv --delete \
   -e "ssh -p \"$STG_SSH_PORT\"" \
-  --exclude "$WORDPRESS_CACHE_DIR_PATH" --exclude "$BACKWPUP_LOG_DIR_PATH" --exclude "$BACKWPUP_TEMP_DIR_PATH" \
+  --exclude "$WORDPRESS_CACHE_DIR_PATH" \
+  --exclude "$BACKWPUP_LOG_DIR_PATH" \
+  --exclude "$BACKWPUP_TEMP_DIR_PATH" \
   "$STG_PUBLIC_DIR_PATH_WITH_DESTINATION"/ "$STG_FILE_BACKUP_DIR_PATH"/
 printf "【完了】\n\n"
+echo "------------------------------ステージング環境バックアップ　完了------------------------------"
 
-echo "【本番のpublic_htmlをステージングにコピー】"
+echo "------------------------------本番→ステージング環境同期　開始------------------------------"
+echo "【DBを同期】"
+ssh "$PRD_SSH_DESTINATION" -p "$PRD_SSH_PORT" \
+  mysqldump \
+  -u"$PRD_DB_USER" \
+  -p"$PRD_DB_PASSWORD" \
+  -h"$PRD_DB_HOST" \
+  "$PRD_DB_NAME" \
+  --no-tablespaces |
+  ssh "$STG_SSH_DESTINATION" -p "$STG_SSH_PORT" \
+    mysql \
+    -u"$STG_DB_USER" \
+    -p"$STG_DB_PASSWORD" \
+    -h"$STG_DB_HOST" \
+    "$STG_DB_NAME"
+printf "【完了】\n\n"
+
+echo "【ドメインを置換】"
+ssh "$STG_SSH_DESTINATION" -p "$STG_SSH_PORT" \
+  /usr/bin/php7.3 srdb.cli.php \
+  -h "$STG_DB_HOST" \
+  -u "$STG_DB_USER" \
+  -p "$STG_DB_PASSWORD" \
+  -n "$STG_DB_NAME" \
+  -s "https://${PRD_DOMAIN}" -r "https://${STG_DOMAIN}"
+printf "【完了】\n\n"
+
+echo "【public_htmlを同期】"
 ssh "$PRD_SSH_DESTINATION" -p "$PRD_SSH_PORT" \
   rsync --checksum -arv --delete \
-  --exclude "$STG_DOMAIN" --exclude "$WORDPRESS_CACHE_DIR_PATH" --exclude "$BACKWPUP_LOG_DIR_PATH" --exclude "$BACKWPUP_TEMP_DIR_PATH" \
+  --exclude "$STG_DOMAIN" \
+  --exclude "$WORDPRESS_CACHE_DIR_PATH" \
+  --exclude "$BACKWPUP_LOG_DIR_PATH" \
+  --exclude "$BACKWPUP_TEMP_DIR_PATH" \
   "$PRD_PUBLIC_DIR_PATH"/ "$STG_PUBLIC_DIR_PATH"
 printf "【完了】\n\n"
 
-echo "【wp-config.phpの内容をステージング環境のものに書き換え】"
+echo "【wp-config.phpを置換】"
 scp -P "$STG_SSH_PORT" \
   ./wp-config-stg.php "$STG_PUBLIC_DIR_PATH_WITH_DESTINATION"/wp-config.php
 printf "【完了】\n\n"
+echo "------------------------------本番→ステージング環境同期　完了------------------------------"
 
-echo "【Basic認証の設定追加】"
+echo "------------------------------後処理　開始------------------------------"
+echo "【Basic認証の設定】"
 ssh "$STG_SSH_DESTINATION" -p "$STG_SSH_PORT" "sed -i '/^\n*$/d' \"$STG_PUBLIC_DIR_PATH/.htaccess\""
 ssh "$STG_SSH_DESTINATION" -p "$STG_SSH_PORT" "echo -e \"\n\" >> \"$STG_PUBLIC_DIR_PATH/.htaccess\""
 ssh <./.htaccess-basic-auth "$STG_SSH_DESTINATION" -p "$STG_SSH_PORT" "cat >> \"$STG_PUBLIC_DIR_PATH\"/.htaccess"
@@ -51,16 +77,7 @@ scp -P "$STG_SSH_PORT" \
   ./.htpasswd "$HTPASSWD_PATH_WITH_DESTINATION"/.htpasswd
 printf "【完了】\n\n"
 
-echo "【ステージングのDBを本番のDBで上書き】"
-ssh "$STG_SSH_DESTINATION" -p "$STG_SSH_PORT" \
-  mysql -u"$STG_DB_USER" -p"$STG_DB_PASSWORD" -h"$STG_DB_HOST" "$STG_DB_NAME" <"$PRD_DB_DUMP_FILE_PATH"
-printf "【完了】\n\n"
-
-echo "【ステージングのDB内のドメイン部分を書き換え】"
-ssh "$STG_SSH_DESTINATION" -p "$STG_SSH_PORT" \
-  /usr/bin/php7.3 srdb.cli.php -h "$STG_DB_HOST" -u "$STG_DB_USER" -p "$STG_DB_PASSWORD" -n "$STG_DB_NAME" -s "https://${PRD_DOMAIN}" -r "https://${STG_DOMAIN}"
-printf "【完了】\n\n"
-
-echo "【本番以外では不要なプラグインの無効化】"
+echo "【ステージング環境では不要なプラグインの無効化】"
 sh deactivate-plugin-stg.sh
 printf "【完了】\n\n"
+echo "------------------------------後処理　完了------------------------------"
