f6349559be8bfe2e75d2595d1a2477666b0440a1
hiroya-akeoka <mowoarm232@gmail.com>
2025-12-14 20:06:26 +0900

    :+1:ブログ固有の設定ファイルを.wp-sync/にコピーするようにした変更に伴うパス指定の修正

--- lib/activate-plugin.sh
+++ lib/activate-plugin.sh
@@ -2,6 +2,6 @@
 # shellcheck source=/dev/null
 # 本番環境でのみ有効化するプラグイン（ステージング→本番同期前にステージング環境で有効化）
 
-. ./.env
+. ./.wp-sync/.env
 
 wp plugin activate $PLUGINS_TO_ACTIVATE --ssh="$STG_SSH_DESTINATION:$STG_SSH_PORT$STG_PUBLIC_DIR_PATH" --allow-root
--- lib/deactivate-plugin.sh
+++ lib/deactivate-plugin.sh
@@ -2,6 +2,6 @@
 # shellcheck source=/dev/null
 # ステージング環境では無効化するプラグイン（本番→ステージング同期後にステージング環境で無効化）
 
-. ./.env
+. ./.wp-sync/.env
 
 wp plugin deactivate $PLUGINS_TO_DEACTIVATE --ssh="$STG_SSH_DESTINATION:$STG_SSH_PORT$STG_PUBLIC_DIR_PATH" --allow-root
