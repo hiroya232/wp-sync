828c0dd1b334682e938e6f503615b3265f3e64ab
hiroya-akeoka <mowoarm232@gmail.com>
2025-12-20 19:59:18 +0900

    📝 設定ファイルテンプレートに説明コメントを追加
    ## 変更内容
    - wp-config-prd.php: 本番環境用設定の説明コメントを追加
    - wp-config-stg.php: ステージング環境用設定の説明コメントを追加
    - .htpasswd: htpasswdコマンドの使い方と記述形式の例を追加
    
    ## 変更理由
    初めて使用するユーザーが各ファイルに何を設定すべきか
    直感的に理解できるようにするため。

---
+++ template/wp-config-prd.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 本番環境用 wp-config.php
+ *
+ * このファイルに本番環境用のWordPress設定を記述してください。
+ * 同期時に本番環境の wp-config.php として置換されます。
+ *
+ * 設定例:
+ * - DB_NAME, DB_USER, DB_PASSWORD, DB_HOST
+ * - WP_DEBUG = false
+ * - その他本番環境固有の設定
+ *
+ * 注意: 実際の wp-config.php の内容をここにコピーして編集してください。
+ */
---
+++ template/wp-config-stg.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * ステージング環境用 wp-config.php
+ *
+ * このファイルにステージング環境用のWordPress設定を記述してください。
+ * 同期時にステージング環境の wp-config.php として置換されます。
+ *
+ * 設定例:
+ * - DB_NAME, DB_USER, DB_PASSWORD, DB_HOST（ステージング用DB）
+ * - WP_DEBUG = true（デバッグ有効化推奨）
+ * - その他ステージング環境固有の設定
+ *
+ * 注意: 実際の wp-config.php の内容をここにコピーして編集してください。
+ */
